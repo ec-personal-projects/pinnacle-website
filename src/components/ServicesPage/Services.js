239c616edc89dcf132a4d645215f75734f74561d
eric-cruz <eacruz@live.com>
2021-06-10 09:22:27 -0500

    Recent Changes - Services

--- src/components/ServicesPage/Services.js
+++ src/components/ServicesPage/Services.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Planning from "../../assets/planning.png";
 
 const Services = () => {
   return (
-    <div className='container services'>
+    <div className="container services">
       <h1>Services</h1>
+      <img src={Planning} alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
